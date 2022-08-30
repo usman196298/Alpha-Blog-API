3cab5dcc17dee90f8c72db4c1d89e17df228ef5b
Muhammad Usman Khan <94914838+usman196298@users.noreply.github.com>
2022-08-30 17:49:59 +0500

    test (#1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # you can also make the above code simple by writing:
   resources :articles
 end
