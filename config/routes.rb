814321916d50e5ec2ad94520052dd7404fab9787
Usman Khan <usman196298@gmail.com>
2022-08-30 17:27:43 +0500

    Deploy application after using DRY approach

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,4 @@ Rails.application.routes.draw do
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # you can also make the above code simple by writing:
   resources :articles
-endc
+end
